8ab4c58aaf0adf2bf9de020ac891766706031027
Irada A. <clan.i4a@gmail.com>
2025-07-24 12:32:45 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -13,3 +13,10 @@ CREATE TABLE comments (
     content TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO shows (title, year, genre, country, description)
+VALUES 
+('Crash Landing on You', 2019, 'Romance', 'Korea', 'Love story between South Korean heiress and North Korean officer'),
+('Attack on Titan', 2013, 'Action', 'Japan', 'Humans vs Titans in post-apocalyptic world'),
+('2gether: The Series', 2020, 'Romance', 'Thailand', 'Fake relationship turns real'),
+('The Untamed', 2019, 'Fantasy', 'China', 'Magical cultivation story');
